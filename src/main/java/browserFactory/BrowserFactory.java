842fb1a9c9028b3d983c798be88d77977a2391c3
Tor-YA <69291928+Tor-YA@users.noreply.github.com>
2023-09-15 13:30:31 +0300

    Added tooltipTest

--- src/main/java/browserFactory/BrowserFactory.java
+++ src/main/java/browserFactory/BrowserFactory.java
@@ -1,30 +1,30 @@
 package browserFactory;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.config.DriverManagerType;
+import utils.configuration.ReadProperties;
 
 import java.time.Duration;
 
 public class BrowserFactory {
-
     private WebDriver driver = null;
     private DriverManagerType driverManagerType = null;
 
     public BrowserFactory(){
-        driverManagerType = DriverManagerType.CHROME;
-        WebDriverManager.chromedriver().driverVersion("114.0.573.98");
-        driver = new ChromeDriver(getChromeOptions());
+            driverManagerType = DriverManagerType.CHROME;
+            WebDriverManager.getInstance(driverManagerType).clearDriverCache().setup();
+            driver = new ChromeDriver(getChromeOptions());
     }
-
 
     public WebDriver getDriver() {
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-
 
         return this.driver;
     }
