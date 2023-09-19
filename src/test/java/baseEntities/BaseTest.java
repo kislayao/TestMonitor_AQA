74a4f3c07aa344f52638fb78dfe2705d5ca4ae73
kislayao <olya.drozd.al@gmail.com>
2023-09-19 22:50:52 +0200

    added making a screenshot on test failure

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -2,13 +2,16 @@ package baseEntities;
 
 import browserFactory.BrowserFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import steps.AddUserStep;
 import steps.LoginStep;
 import steps.UsersStep;
 import utils.configuration.ReadProperties;
 
+@Listeners(InvokedListener.class)
 public class BaseTest {
     protected WebDriver driver;
 
@@ -17,13 +20,19 @@ public class BaseTest {
     protected AddUserStep addUserStep;
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp(ITestContext iTestContext) {
         BrowserFactory browserFactory = new BrowserFactory();
         driver = browserFactory.getDriver();
+        this.setDriverToContext(iTestContext, driver);
+
         loginStep = new LoginStep(driver);
         usersStep = new UsersStep(driver);
         addUserStep = new AddUserStep(driver);
         driver.get(ReadProperties.getUrl());
+    }
+
+    private void setDriverToContext(ITestContext iTestContext, WebDriver driver) {
+        iTestContext.setAttribute("WebDriver", driver);
     }
 
     @AfterMethod
--- src/test/java/tests/ui/FailedOnPurposeTest.java
+++ src/test/java/tests/ui/FailedOnPurposeTest.java
@@ -2,12 +2,17 @@ package tests.ui;
 
 import baseEntities.BaseTest;
 import helper.DataHelper;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FailedOnPurposeTest extends BaseTest {
 
-    @Test
+    @Test (description = "Failed on purpose test")
+    @Description("This test if failed on purpose")
+    @Severity(SeverityLevel.MINOR)
     public void failedTest(){
         Assert.assertFalse(loginStep.successLogin(DataHelper.getUserToLogin()).isPageOpened());
     }
